059a9033a8c3493473d4c9d687f044e827d7ed9e
Toussaint CARLOTTI <toussaint.carlotti@gmail.com>
2023-12-16 21:57:37 +0100

    Modification des vols par avion

--- app/Http/Controllers/FlightByAircraftController.php
+++ app/Http/Controllers/FlightByAircraftController.php
@@ -9,9 +9,10 @@ class FlightByAircraftController extends Controller
 {
     public function __invoke(string $icao24, string $begin, string $end)
     {
-        $flights = Flight::where('icao24', $icao24)
+        $flight = Flight::where('icao24', $icao24)->firstOrFail();
+        $flights = Flight::where('estDepartureAirport', $flight->estDepartureAirport)
             ->where('firstSeen', '>=', $begin)
-            ->where('firstSeen', '<=', $end)
+            ->where('lastSeen', '<=', $end)
             ->get();
 
         return FlightResource::collection($flights);
